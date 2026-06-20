8b00ec3e37c33193aecc0352057d63b072f3542a
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2026-06-20 18:28:46 +0700

    Add cors middleware

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import pipeline
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 classifier = pipeline(
     "text-classification",
