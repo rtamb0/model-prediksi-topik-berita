50dcc28206ae781f4d09e442d923da81b16b884b
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2026-06-20 18:04:47 +0700

    Implement bootstrap

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "../dist",
+  },
+  server: {
+    port: 8080,
+  },
+  // Optional: Silence Sass deprecation warnings. See note below.
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          "import",
+          "mixed-decls",
+          "color-functions",
+          "global-builtin",
+        ],
+      },
+    },
+  },
+});
